4bed2e4128d7cd171a3b9aa6ab3d96add6afd541
lyla c. <mimi@awawa.foo>
2025-05-18 18:18:47 -0400

    fix build

--- crates/cascade-app/build.rs
+++ crates/cascade-app/build.rs
@@ -1,7 +1,7 @@
 #[cfg(windows)]
 fn main() {
     let mut res = winres::WindowsResource::new();
-    res.set_icon("../assets/cascade.ico");
+    res.set_icon("../../assets/cascade.ico");
     res.compile().unwrap();
 }
 
